0a382590f6c9ce96bac4575f80c5d69710a0ce54
Seb <sebwiggins@me.com>
2021-08-27 10:56:32 +0100

    adding semantic-ui

---
+++ src/components/AppHeader.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+
+const AppHeader = () => {
+
+
+
+    return (
+        <Fragment>
+              <Header size='huge'>1RM Caclulator</Header>
+        </Fragment>
+    )
+}
+
+export default AppHeader
